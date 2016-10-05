8bc47317457c9c0429142bddea9702608db9da1e
Fabian Schneider <fabischn@student.ethz.ch>
2016-10-05 19:06:22 +0200

    Wrote some boiler plate code

--- app/src/main/java/ch/ethz/inf/vs/a1/fabischn/sensors/MainActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/fabischn/sensors/MainActivity.java
@@ -1,13 +1,44 @@
 package ch.ethz.inf.vs.a1.fabischn.sensors;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ListView.OnItemClickListener{
+
+    private SensorManager mSensorManager = null;
+    private List<Sensor> mSensors = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.list_sensors);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        listView.setAdapter(new ArrayAdapter<Sensor>(this,R.layout.listview_item,mSensors));
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent sensorIntent = new Intent(this, SensorActivity.class);
+        Sensor sensor = (Sensor) parent.getAdapter().getItem(position);
+        String sensorName = sensor.getName();
+        sensorIntent.putExtra(Intent.EXTRA_TEXT, sensorName);
+        startActivity(sensorIntent);
     }
 }
+
+
+
--- app/src/main/java/ch/ethz/inf/vs/a1/fabischn/sensors/SensorActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/fabischn/sensors/SensorActivity.java
@@ -1,14 +1,21 @@
 package ch.ethz.inf.vs.a1.fabischn.sensors;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SensorActivity extends AppCompatActivity {
 
+    private TextView mTextViewSensorName = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+        mTextViewSensorName = (TextView) findViewById(R.id.text_sensor);
+        Intent intent = getIntent();
+        String sensorName = intent.getStringExtra(Intent.EXTRA_TEXT);
+        mTextViewSensorName.setText(sensorName);
     }
 
     public GraphContainer getGraphContainer(){
